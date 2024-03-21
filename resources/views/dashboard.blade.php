60f039b51fc24cea918924be58d0b716c543561d
bowoxdaily <ikhwanudinaldi04@gmail.com>
2024-03-21 11:28:34 +0700

    Dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+
+@section('Section', 'Dashboard')
+
+@section('content')
+
+    <main id="main" class="main">
+        <section class="section dashboard">
+
+            <h1>awowaowka</h1>
+        </section>
+    </main>
+
+@endsection
